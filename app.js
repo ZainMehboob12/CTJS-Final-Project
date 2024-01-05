18e2fcb0ff0e1d91f1ea3211d45c8de81d9969ad
Zain Mehboob <147308096+ZainMehboob12@users.noreply.github.com>
2024-01-05 07:15:40 +0500

    crousel added for full screen

---
+++ app.js
@@ -0,0 +1,10 @@
+window.addEventListener('resize', () => {
+    // Recalculate carousel and slide dimensions if needed
+    const carousel = document.getElementById('carouselExampleSlidesOnly');
+    carousel.style.height = '100vh';
+    carousel.style.width = '100vw';
+});
+
+
+//position in page caption
+
