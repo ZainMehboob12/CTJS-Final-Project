87a50f70b014e5d671b9fdaa2925e103061f27a4
Zain Mehboob <147308096+ZainMehboob12@users.noreply.github.com>
2024-01-05 10:23:58 +0500

    sb change kia pehly waly se

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener('resize', () => {
     // Recalculate carousel and slide dimensions if needed
-    const carousel = document.getElementById('carouselExampleCaptions');
+    const carousel = document.getElementById('carousel-example-2');
     carousel.style.height = '100vh';
     carousel.style.width = '100vw';
 });
