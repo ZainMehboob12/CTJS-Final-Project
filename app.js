f8b9adf2d1c6e5e1f6904593580b63ea0b92b968
Zain Mehboob <147308096+ZainMehboob12@users.noreply.github.com>
2024-01-05 07:27:33 +0500

    change autoslide to button mode

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 window.addEventListener('resize', () => {
     // Recalculate carousel and slide dimensions if needed
-    const carousel = document.getElementById('carouselExampleSlidesOnly');
+    const carousel = document.getElementById('carouselExampleCaptions');
     carousel.style.height = '100vh';
     carousel.style.width = '100vw';
 });
